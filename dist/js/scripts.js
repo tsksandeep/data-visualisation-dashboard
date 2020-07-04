15582fd7ddf44039151d0207a1ceb572b80fb00f
tsksandeep <tsksandeep11@gmail.com>
2020-07-04 12:38:53 +0530

    Removed unnecessary files

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,9 +1,4 @@
-/*!
-    * Start Bootstrap - SB Admin v6.0.1 (https://startbootstrap.com/templates/sb-admin)
-    * Copyright 2013-2020 Start Bootstrap
-    * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-sb-admin/blob/master/LICENSE)
-    */
-    (function($) {
+(function($) {
     "use strict";
 
     // Add active state to sidbar nav links
