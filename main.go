3fb378ee67a9d3e2e2c2c9cf803c1988f56f88cc
tsksandeep <tsksandeep11@gmail.com>
2020-07-04 19:03:59 +0530

    Minor change

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func GetPort() string {
 	// Set a default port if there is nothing in the environment
 	if port == "" {
 		port = "3000"
-		fmt.Println("INFO: No PORT environment variable detected, defaulting to " + port)
+		log.Println("INFO: No PORT environment variable detected, defaulting to " + port)
 	}
 	return ":" + port
 }
