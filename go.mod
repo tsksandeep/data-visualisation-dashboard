b6957c3180493ddafe34b61ad1ae0035b918a961
tsksandeep <tsksandeep11@gmail.com>
2020-07-05 20:48:12 +0530

    Added ordered dict

--- go.mod
+++ go.mod
@@ -4,8 +4,9 @@ require (
 	github.com/go-chi/chi v4.1.2+incompatible
 	github.com/go-chi/cors v1.1.1
 	github.com/gorilla/sessions v1.2.0
+	github.com/iancoleman/orderedmap v0.0.0-20190318233801-ac98e3ecb4b0
 	github.com/sirupsen/logrus v1.6.0
-	github.com/stretchr/testify v1.3.0 // indirect
+	github.com/stretchr/testify v1.4.0 // indirect
 )
 
 go 1.13
--- handlers/data/data.go
+++ handlers/data/data.go
@@ -5,6 +5,8 @@ import (
 	"net/http"
 
 	"know/handlers"
+
+	"github.com/iancoleman/orderedmap"
 )
 
 type dataHandler struct{}
@@ -15,118 +17,93 @@ func New() handlers.DataHandler {
 }
 
 func (dh *dataHandler) GetEmployeeData(w http.ResponseWriter, r *http.Request) {
-	employeeData := map[string][]map[string]string{}
+	employeeData := orderedmap.New()
 
-	employeeData["employee"] = []map[string]string{
-		{
-			"Name":       "Tiger Nixon",
-			"Position":   "System Architect",
-			"Office":     "Edinburgh",
-			"Age":        "61",
-			"Start Date": "2011/04/25",
-			"Salary":     "$320,800",
-		},
-		{
-			"Name":       "Cedric Kelly",
-			"Position":   "Senior Javascript Developer",
-			"Office":     "Edinburgh",
-			"Age":        "22",
-			"Start Date": "2012/03/29",
-			"Salary":     "$433,060",
-		},
-		{
-			"Name":       "Ashton Cox",
-			"Position":   "Junior Technical Author",
-			"Office":     "San Francisco",
-			"Age":        "66",
-			"Start Date": "2009/01/12",
-			"Salary":     "$86,000",
-		},
-		{
-			"Name":       "Garrett Winters",
-			"Position":   "Accountant",
-			"Office":     "Tokyo",
-			"Age":        "63",
-			"Start Date": "2011/07/25",
-			"Salary":     "$170,750",
-		},
-		{
-			"Name":       "Tiger Nixon",
-			"Position":   "System Architect",
-			"Office":     "Edinburgh",
-			"Age":        "61",
-			"Start Date": "2011/04/25",
-			"Salary":     "$320,800",
-		},
-		{
-			"Name":       "Cedric Kelly",
-			"Position":   "Senior Javascript Developer",
-			"Office":     "Edinburgh",
-			"Age":        "22",
-			"Start Date": "2012/03/29",
-			"Salary":     "$433,060",
-		},
-		{
-			"Name":       "Ashton Cox",
-			"Position":   "Junior Technical Author",
-			"Office":     "San Francisco",
-			"Age":        "66",
-			"Start Date": "2009/01/12",
-			"Salary":     "$86,000",
-		},
-		{
-			"Name":       "Garrett Winters",
-			"Position":   "Accountant",
-			"Office":     "Tokyo",
-			"Age":        "63",
-			"Start Date": "2011/07/25",
-			"Salary":     "$170,750",
-		},
-	}
+	employee1 := orderedmap.New()
+	employee1.Set("Name", "Tiger Nixon")
+	employee1.Set("Position", "System Architect")
+	employee1.Set("Office", "Edinburgh")
+	employee1.Set("Age", "61")
+	employee1.Set("Start Date", "2011/04/25")
+	employee1.Set("Salary", "$320,800")
+
+	employee2 := orderedmap.New()
+	employee2.Set("Name", "Cedric Kelly")
+	employee2.Set("Position", "Senior Javascript Developer")
+	employee2.Set("Office", "Edinburgh")
+	employee2.Set("Age", "22")
+	employee2.Set("Start Date", "2012/03/29")
+	employee2.Set("Salary", "$433,060")
+
+	employee3 := orderedmap.New()
+	employee3.Set("Name", "Ashton Cox")
+	employee3.Set("Position", "Junior Technical Author")
+	employee3.Set("Office", "San Francisco")
+	employee3.Set("Age", "66")
+	employee3.Set("Start Date", "2009/01/12")
+	employee3.Set("Salary", "$86,000")
+
+	employee4 := orderedmap.New()
+	employee4.Set("Name", "Garrett Winters")
+	employee4.Set("Position", "Accountant")
+	employee4.Set("Office", "Tokyo")
+	employee4.Set("Age", "63")
+	employee4.Set("Start Date", "2011/07/25")
+	employee4.Set("Salary", "$170,750")
+
+	employeeData.Set("employee", []orderedmap.OrderedMap{
+		*employee1,
+		*employee2,
+		*employee3,
+		*employee4,
+	})
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(employeeData)
 }
 
 func (dh *dataHandler) GetMonthData(w http.ResponseWriter, r *http.Request) {
-	monthData := map[string]int{
-		"January":30000,
-		"February":20000,
-		"March":45000,
-		"April":27000,
-		"May":15000,
-		"June":35000,
-	}
+	monthData := orderedmap.New()
+
+	monthData.Set("January", 30000)
+	monthData.Set("February", 20000)
+	monthData.Set("March", 45000)
+	monthData.Set("April", 27000)
+	monthData.Set("May", 15000)
+	monthData.Set("June", 35000)
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(monthData)
 }
 
 func (dh *dataHandler) GetDayData(w http.ResponseWriter, r *http.Request) {
-	dayData := map[string]int{
-		"June 1":30000,
-		"June 2":20000,
-		"June 3":45000,
-		"June 4":65000,
-		"June 5":15000,
-		"June 6":27000,
-		"June 7":34000,
-		"June 8":45000,
-		"June 9":35000,
-		"June 10":45000,
-	}
+	dayData := orderedmap.New()
+
+	dayData.Set("June 1", 30000)
+	dayData.Set("June 2", 20000)
+	dayData.Set("June 3", 45000)
+	dayData.Set("June 4", 31000)
+	dayData.Set("June 5", 15000)
+	dayData.Set("June 6", 27000)
+	dayData.Set("June 7", 34000)
+	dayData.Set("June 8", 45000)
+	dayData.Set("June 9", 35000)
+	dayData.Set("June 10", 45000)
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(dayData)
 }
 
 func (dh *dataHandler) GetProfitData(w http.ResponseWriter, r *http.Request) {
-	profitData := map[string]int{
-		"January":30000,
-		"February":20000,
-		"March":45000,
-		"April":27000,
-		"May":15000,
-		"June":35000,
-	}
+	profitData := orderedmap.New()
+
+	profitData.Set("January", 30000)
+	profitData.Set("February", 20000)
+	profitData.Set("March", 45000)
+	profitData.Set("April", 27000)
+	profitData.Set("May", 15000)
+	profitData.Set("June", 35000)
+
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(profitData)
 }
